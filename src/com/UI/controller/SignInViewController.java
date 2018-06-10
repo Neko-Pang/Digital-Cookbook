e4a74f00d83618275cd0acaf1a5daa146ba94fb3
QingDerui <rose@windows10.microdone.cn>
2018-06-10 12:20:40 +0200

    sign in finish / insert not

--- src/com/UI/controller/SignInViewController.java
+++ src/com/UI/controller/SignInViewController.java
@@ -1,9 +1,14 @@
 package com.UI.controller;
 
+import java.awt.Color;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.security.auth.callback.ConfirmationCallback;
+
+import com.mysql.jdbc.SocketMetadata.Helper;
 
 import CookBook.DatabaseController;
 import javafx.fxml.FXML;
@@ -13,14 +18,36 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-public class SignInViewController implements Initializable{
+public class SignInViewController extends Thread implements Initializable{
 	
 	@FXML
 	private TextField usernameText;
 	@FXML
 	private Label usernameCheckHint;
 	@FXML
+	private Label usernameCheckHint2;
+	@FXML
 	private Button cancelButton;
+	@FXML
+	private TextField passwordText;
+	@FXML
+	private TextField passwordRepeat;
+	@FXML
+	private Label passwordCheckHint;
+	@FXML
+	private Label passwordCheckHint2;
+	@FXML
+	private Button confirmButton;
+	
+	
+	
+	private String password;
+	private String repeatPassword;
+	private boolean passwordCheckLengthPass = false;
+	private boolean usernamePass = false;
+	private boolean passwordPass = false;
+	
+	private boolean stopme = true;
 	
 	DatabaseController jdbc = DatabaseController.getInstance();
 	
@@ -30,32 +57,165 @@ public class SignInViewController implements Initializable{
 		
 		usernameText.setOnKeyReleased(e->userNameCheck());
 		cancelButton.setOnAction(e->MainController.getSubStage().close());
+		passwordText.setOnKeyReleased(e->passwordCheck());
+		passwordRepeat.setOnKeyReleased(e->passwordRepeatCheck());	
+		confirmButton.setOnAction(e->comfirm());
+		this.start();
+		
 	}
 	
 	
+	/**
+	 * this method is to check whether the username is legal
+	 */
 	public void userNameCheck(){
 		
 		String username = usernameText.getText();
+		int maxLength = 30;
 		Pattern pattern = Pattern.compile("[0-9a-zA-Z]+");
 		Matcher matcher = pattern.matcher(username);
 		boolean isRight = matcher.matches();
+		
+		if(username.length() > maxLength){
+			
+			String s = username.substring(0, maxLength);
+			username = s;
+			usernameText.setText(username);
+			usernameText.positionCaret(maxLength);
+		}
 		
 		if(!username.equals("")){
 			if(isRight){
 				if(jdbc.searchUser(username) == null){
 			
-					usernameCheckHint.setText("Username is valid ¡Ì");
-			
+					usernameCheckHint.setText("Username is valid ");
+					usernameCheckHint2.setText("¡Ì");
+					usernameCheckHint2.setTextFill(javafx.scene.paint.Color.GREEN);
+					usernamePass = true;
+					
 				}else{
-					usernameCheckHint.setText("Username is repeated X");
+					usernameCheckHint.setText("Username is repeated ");
+					usernameCheckHint2.setText("X");
+					usernameCheckHint2.setTextFill(javafx.scene.paint.Color.RED);
+					usernamePass = false;
 				}
 			}else{
-				usernameCheckHint.setText("Username is invalid X");
+				usernameCheckHint.setText("Username is invalid ");
+				usernameCheckHint2.setText("X");
+				usernameCheckHint2.setTextFill(javafx.scene.paint.Color.RED);
+				usernamePass = false;
 			}
 		}else{
 			usernameCheckHint.setText(" ");
+			usernameCheckHint2.setText(" ");
+			usernamePass = false;
 		}
 		
+		
+	}
+	
+	
+	public void passwordCheck(){
+		
+		String password = passwordText.getText();
+		
+		int maxLength = 16;
+		int minLength = 8;
+		if(password.length() > maxLength){
+			
+			String s = password.substring(0, maxLength);
+			password = s;
+			passwordText.setText(password);
+			passwordText.positionCaret(maxLength);
+		}
+		
+		
+		if(password.length() < minLength){
+			passwordCheckHint.setText("Password is too short ");
+			passwordCheckHint2.setText("X");
+			passwordCheckHint2.setTextFill(javafx.scene.paint.Color.RED);
+			passwordCheckLengthPass=false;
+			passwordPass = false;
+		}else if( !password.equals(this.repeatPassword) ){
+			passwordCheckHint.setText("Please input the same password ");
+			passwordCheckHint2.setText("X");
+			passwordCheckHint2.setTextFill(javafx.scene.paint.Color.RED);	
+			passwordCheckLengthPass=true;
+			passwordPass = false;
+		}else{
+			passwordCheckHint.setText("Password is valid ");
+			passwordCheckHint2.setText("¡Ì");
+			passwordCheckHint2.setTextFill(javafx.scene.paint.Color.GREEN);
+			passwordCheckLengthPass=true;
+			passwordPass = true;
+		}
+		
+		this.password=password;
+
+		System.out.println(this.password);
+		
+	}
+	
+	public void passwordRepeatCheck(){
+		
+		
+		String passwordRepeat = this.passwordRepeat.getText();
+		
+		int maxLength = 16;
+		if(passwordCheckLengthPass){
+			if (passwordRepeat.length() > maxLength) {
+
+				String s = passwordRepeat.substring(0, maxLength);
+				passwordRepeat = s;
+				this.passwordRepeat.setText(passwordRepeat);
+				this.passwordRepeat.positionCaret(maxLength);
+			}
+
+			if (passwordRepeat.equals(password)) {
+				passwordCheckHint.setText("Password is valid ");
+				passwordCheckHint2.setText("¡Ì");
+				passwordCheckHint2.setTextFill(javafx.scene.paint.Color.GREEN);
+				passwordPass = true;
+			} else {
+
+				passwordCheckHint.setText("Please input the same password ");
+				passwordCheckHint2.setText("X");
+				passwordCheckHint2.setTextFill(javafx.scene.paint.Color.RED);
+				passwordPass = false;
+
+			}
+
+		}
+		
+		this.repeatPassword = passwordRepeat;
+		
+		System.out.println(repeatPassword);
+	}
+
+
+	public void comfirm(){
+		
+		MainController.getSubStage().close();
+		stopme = false;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		while(stopme){
+			
+			if(passwordPass && usernamePass){
+				
+				confirmButton.setDisable(false);
+				
+			}else{
+
+				confirmButton.setDisable(true);
+				
+			}
+			
+		}
 		
 	}
 	
